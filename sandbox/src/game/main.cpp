75c8899487e5eddd5aa3291d6bf0372b579d1cd6
blostic <piotr.skibiak@gmail.com>
2014-09-06 11:26:06 +0200

    Fishes

--- sandbox/src/game/main.cpp
+++ sandbox/src/game/main.cpp
@@ -1,10 +1,13 @@
 #include <fenv.h>
 
+// #include <cstdlib> 
+// #include <ctime> 
 #include "window/window.h"
 #include "rendering/sprite.h"
 #include "rendering/line.h"
 #include "rendering/model.h"
 #include "rendering/string.h"
+#include "rendering/fish.h"
 #include "rendering/terrain.h"
 #include "utils/logger.h"
 #include "utils/stringUtils.h"
@@ -55,6 +58,129 @@ public:
     bool running() const { return mRunning; }
 };
 
+std::vector<sb::Fish> shoalOfFish;
+
+void initializeSwarmModelSet(int size, std::shared_ptr<sb::Shader> textureShader)
+{
+    srand((unsigned)time(0));
+    for (int i = 0; i < size; ++i)
+    {
+            sb::Fish fish("salamon.obj",
+                     textureShader, gResourceMgr.getTexture("salamon2.jpg"));
+            fish.setPosition(rand() % 20 - 10.0, rand() % 20 - 10.0, rand() % 20 - 10.0);
+            fish.setVelocity((rand() % 50 - 25)/50.0, (rand() % 50 - 25)/50.0, (rand() % 50 - 25)/50.0);
+            fish.setScale(0.8f);
+            shoalOfFish.push_back(fish);
+    }
+} 
+
+Vec3 mass_rule(sb::Fish fish)
+{
+    Vec3 m_centrum(0.f,0.f, 0.f);
+    
+    for(sb::Fish fish_it : shoalOfFish)
+    {
+            m_centrum = m_centrum + fish_it.getPosition();
+    }
+    m_centrum = m_centrum - fish.getPosition();   
+    m_centrum = m_centrum / ( shoalOfFish.size() - 1.0);
+
+    return (m_centrum - fish.getPosition()) / 10000;
+}
+
+double distance(Vec3 A, Vec3 B)
+{
+    return sqrt( (A.x - B.x)*(A.x - B.x) + (A.y - B.y)*(A.y - B.y) + (A.z - B.z)*(A.z - B.z));
+}
+
+Vec3 not_so_close_rule(sb::Fish fish)
+{
+    Vec3 closeness_vector(0.f,0.f, 0.f);
+    
+    for(sb::Fish fish_it : shoalOfFish)
+    {
+        if (distance(fish_it.getPosition(), fish.getPosition()) < 0.5) {
+            closeness_vector = closeness_vector - (fish_it.getPosition() - fish.getPosition())/100; 
+        }
+    }
+
+    return closeness_vector;   
+}
+
+Vec3 similar_velocity_rule(sb::Fish fish)
+{
+    Vec3 perceived_velocity(0.f,0.f, 0.f);
+
+    for(sb::Fish fish_it : shoalOfFish)
+    {
+        perceived_velocity = perceived_velocity + fish_it.getVelocity(); 
+    }
+
+    perceived_velocity = perceived_velocity - fish.getVelocity();
+    perceived_velocity = perceived_velocity / ( shoalOfFish.size() - 1);
+    
+    return (perceived_velocity - fish.getVelocity()) / 100;
+}
+
+Vec3 tend_to_place(sb::Fish fish, Vec3 place)
+{
+    return (place - fish.getPosition()) / 100000;
+}
+
+Vec3 not_so_fast(sb::Fish fish, float max_speed)
+{
+    if (distance(Vec3(0.f, 0.f, 0.f), fish.getPosition()) > max_speed)
+    {
+        return -fish.getVelocity() / 2;
+    }
+    return Vec3(0.f, 0.f, 0.f);
+}
+
+Vec3 avoid_predator(sb::Fish fish, Vec3 predator_position)
+{
+    Vec3 avoid_predator_velocity(0.f,0.f, 0.f);
+    if (distance(predator_position, fish.getPosition()) < 3.0f)
+    {
+        return (fish.getPosition() - predator_position) * 0.005f;
+    }
+    return avoid_predator_velocity;
+}
+
+
+Vec3 calculateVelocity(sb::Fish& fish, Vec3 predator_position)
+{
+    Vec3 before = {0.f, 0.f, 1.f}; 
+    Vec3 after = fish.getVelocity() + tend_to_place(fish, Vec3(0.f, 1.f, 1.f)) + mass_rule(fish)  + not_so_close_rule(fish) + similar_velocity_rule(fish) + avoid_predator(fish, predator_position);
+    Vec3 rotation_axis = before.cross(fish.getVelocity());    
+    float rotation_angle = acos(before.dot(fish.getVelocity()));
+    
+    if(! rotation_axis.isZero()){
+        fish.setRotation(rotation_axis, Radians(rotation_angle));
+    }
+         
+    return after;
+}
+
+int conuter = 0;
+
+void animateFish(sb::Fish& fish)
+{  
+    // conuter++;
+    // if (conuter % 7 == 0) {
+    //     Vec3 source = fish.getRotationAxis();
+    //     Vec3 dest = fish.getVelocity();
+    //     Vec3 rotAxis = source.cross(dest).normalized();
+    //     float rotAngle = acos(source.normalized().dot(dest.normalized())); 
+    //     fish.setRotation(rotAxis, Radians(rotAngle/8));
+    // }
+
+}
+
+Vec3 calculatePosition(sb::Fish fish)
+{
+    return fish.getVelocity() + fish.getPosition();
+}
+
 int main()
 {
     feenableexcept(FE_INVALID | FE_DIVBYZERO | FE_OVERFLOW | FE_UNDERFLOW);
@@ -71,38 +197,43 @@ int main()
     wnd.lockCursor();
     wnd.hideCursor();
 
-    Sim::Simulation sim(Sim::Simulation::SimSingleThrow,
-                        textureLightShader,
-                        colorShader);
-    sim.setThrowStart(Vec3d(0., 1., 0.), Vec3d(30., 30., 0.));
+    // Sim::Simulation sim(Sim::Simulation::SimSingleThrow,
+    //                     textureLightShader,
+    //                     colorShader);
+    // sim.setThrowStart(Vec3d(0., 1., 0.), Vec3d(30., 30., 0.));
 
-    sb::Sprite crosshair("dot.png", textureShader);
-    crosshair.setPosition(0.f, 0.f, 0.f);
-    crosshair.setScale(0.01f, 0.01f * 1.33f, 1.f);
+    // sb::Sprite crosshair("dot.png", textureShader);
+    // crosshair.setPosition(0.f, 0.f, 0.f);
+    // crosshair.setScale(0.01f, 0.01f * 1.33f, 1.f);
 
-    sb::Line xaxis(Vec3(1000.f, 0.f, 0.f),
-                   sb::Color(0.f, 0.f, 1.f),
-                   colorShader);
-    sb::Line yaxis(Vec3(0.f, 1000.f, 0.f),
-                   sb::Color(1.f, 0.f, 0.f),
-                   colorShader);
-    sb::Line zaxis(Vec3(0.f, 0.f, 1000.f),
-                   sb::Color(0.f, 1.f, 0.f),
-                   colorShader);
+    // sb::Line xaxis(Vec3(1000.f, 0.f, 0.f),
+    //                sb::Color(0.f, 0.f, 1.f),
+    //                colorShader);
+    // sb::Line yaxis(Vec3(0.f, 1000.f, 0.f),
+    //                sb::Color(1.f, 0.f, 0.f),
+    //                colorShader);
+    // sb::Line zaxis(Vec3(0.f, 0.f, 1000.f),
+    //                sb::Color(0.f, 1.f, 0.f),
+    //                colorShader);
 
     sb::Model skybox("skybox.obj",
                      textureShader,
                      gResourceMgr.getTexture("miramar.jpg"));
     skybox.setScale(1000.f);
 
-    sb::Model tower("Tower_Mats.obj",
-                     textureShader,
-                     gResourceMgr.getTexture("miramar.jpg"));
-    tower.setPosition(0.f, 0.f, 0.f);
+    // sb::Model shark("dragon.obj",
+    //                  textureShader, gResourceMgr.getTexture("salamon.jpg"));
+    // shark.setScale(3.f);
+
+    initializeSwarmModelSet(150, textureShader);
+    // sb::Model tower("sphere.obj",
+    //                  textureShader,
+    //                  gResourceMgr.getTexture("miramar.jpg"));
+    // tower.setPosition(0.f, 0.f, 0.f);
 
     sb::Terrain terrain("hmap_flat.jpg", "ground.jpg", textureShader);
     terrain.setScale(10.f, 1.f, 10.f);
-    terrain.setPosition(-640.f, 0.f, -640.f);
+    terrain.setPosition(-640.f, -30.f, -640.f);
 
     gLog.info("all data loaded!\n");
 
@@ -113,7 +244,7 @@ int main()
 
     sb::Timer clock;
     float lastFrameTime = 0.f;
-    float physicsUpdateStep = 0.03f;
+    // float physicsUpdateStep = 0.03f;
     float fpsCounter = 0.f;      // how many frames have been rendered in fpsUpdateStep time?
     float fpsUpdateStep = 1.f;   // update FPS-string every fpsUpdateStep seconds
     float fpsCurrValue = 0.f;    // current FPS value
@@ -129,8 +260,8 @@ int main()
     std::string fpsString;
 
     bool displayHelp = true;
-    bool displaySimInfo = true;
-    bool displayBallInfo = true;
+    // bool displaySimInfo = true;
+    // bool displayBallInfo = true;
 
     const std::string helpString =
         "controls:\n"
@@ -193,14 +324,14 @@ int main()
                     switch (e.data.mouse.button)
                     {
                     case sb::Mouse::ButtonLeft:
-                        if (!throwVelocity.running()) {
-                            throwVelocity.reset();
-                        }
+                        // if (!throwVelocity.running()) {
+                        //     throwVelocity.reset();
+                        // }
                         break;
                     case sb::Mouse::ButtonRight:
-                        if (!windVelocity.running()) {
-                            windVelocity.reset();
-                        }
+                        // if (!windVelocity.running()) {
+                        //     windVelocity.reset();
+                        // }
                         break;
                     default:
                         break;
@@ -213,23 +344,23 @@ int main()
                     {
                     case sb::Mouse::ButtonLeft:
                         {
-                            Vec3 pos = wnd.getCamera().getEye();
-                            if (pos.y < sim.getBallRadius()) {
-                                pos.y = (float)sim.getBallRadius();
-                            }
+                            // Vec3 pos = wnd.getCamera().getEye();
+                            // if (pos.y < sim.getBallRadius()) {
+                            //     pos.y = (float)sim.getBallRadius();
+                            // }
 
-                            Vec3 v = wnd.getCamera().getFront().normalized()
-                                                    * throwVelocity.getValue();
-                            sim.setThrowStart(Vec3d(pos), Vec3d(v));
-                            sim.reset();
-                            throwVelocity.stop();
+                            // Vec3 v = wnd.getCamera().getFront().normalized()
+                            //                         * throwVelocity.getValue();
+                            // sim.setThrowStart(Vec3d(pos), Vec3d(v));
+                            // sim.reset();
+                            // throwVelocity.stop();
                             break;
                         }
                         break;
                     case sb::Mouse::ButtonRight:
-                        sim.setWind(Vec3d(wnd.getCamera().getFront().normalized()
-                                          * windVelocity.getValue()));
-                        windVelocity.stop();
+                        // sim.setWind(Vec3d(wnd.getCamera().getFront().normalized()
+                        //                   * windVelocity.getValue()));
+                        // windVelocity.stop();
                         break;
                     default:
                         break;
@@ -271,28 +402,28 @@ int main()
                                                Vec3(0.f, 0.f, 0.f));
                         break;
                     case sb::Key::N:
-                        sim.increaseBallPathLength(-1.0);
+                        // sim.increaseBallPathLength(-1.0);
                         break;
                     case sb::Key::J:
-                        sim.increaseBallPathLength(1.0);
+                        // sim.increaseBallPathLength(1.0);
                         break;
                     case sb::Key::M:
-                        sim.increaseBallThrowDelay(-0.1f);
+                        // sim.increaseBallThrowDelay(-0.1f);
                         break;
                     case sb::Key::K:
-                        sim.increaseBallThrowDelay(0.1f);
+                        // sim.increaseBallThrowDelay(0.1f);
                         break;
                     case sb::Key::B:
-                        sim.increaseAirDensity(-0.01);
+                        // sim.increaseAirDensity(-0.01);
                         break;
                     case sb::Key::H:
-                        sim.increaseAirDensity(0.01);
+                        // sim.increaseAirDensity(0.01);
                         break;
                     case sb::Key::Comma:
-                        sim.increaseMaxBalls(-5);
+                        // sim.increaseMaxBalls(-5);
                         break;
                     case sb::Key::L:
-                        sim.increaseMaxBalls(5);
+                        // sim.increaseMaxBalls(5);
                         break;
                     case sb::Key::A: strafeSpeed = -SPEED; break;
                     case sb::Key::D: strafeSpeed = SPEED; break;
@@ -301,22 +432,22 @@ int main()
                     case sb::Key::Q: ascendSpeed = SPEED; break;
                     case sb::Key::Z: ascendSpeed = -SPEED; break;
                     case sb::Key::C:
-                        sim.increaseSloMoFactor(-0.1f);
+                        // sim.increaseSloMoFactor(-0.1f);
                         break;
                     case sb::Key::F:
-                        sim.increaseSloMoFactor(0.1f);
+                        // sim.increaseSloMoFactor(0.1f);
                         break;
                     case sb::Key::Slash:
-                        sim.increaseBallRadius(-0.1f);
+                        // sim.increaseBallRadius(-0.1f);
                         break;
                     case sb::Key::Apostrophe:
-                        sim.increaseBallRadius(0.1f);
+                        // sim.increaseBallRadius(0.1f);
                         break;
                     case sb::Key::Period:
-                        sim.increaseBallMass(-0.1f);
+                        // sim.increaseBallMass(-0.1f);
                         break;
                     case sb::Key::Colon:
-                        sim.increaseBallMass(0.1f);
+                        // sim.increaseBallMass(0.1f);
                         break;
                     case sb::Key::Esc:
                         wnd.close();
@@ -343,23 +474,23 @@ int main()
                         ascendSpeed = 0.f;
                         break;
                     case sb::Key::P:
-                        sim.togglePause();
+                        // sim.togglePause();
                         break;
                     case sb::Key::O:
-                        sim.toggleVectorDisplayType();
+                        // sim.toggleVectorDisplayType();
                         break;
                     case sb::Key::I:
-                        sim.togglePauseOnGroundHit();
+                        // sim.togglePauseOnGroundHit();
                         break;
                     case sb::Key::R:
-                        sim = Sim::Simulation(Sim::Simulation::SimContiniousThrow,
-                                              textureShader,
-                                              colorShader);
+                        // sim = Sim::Simulation(Sim::Simulation::SimContiniousThrow,
+                        //                       textureShader,
+                        //                       colorShader);
                         break;
                     case sb::Key::T:
-                        sim = Sim::Simulation(Sim::Simulation::SimSingleThrow,
-                                              textureShader,
-                                              colorShader);
+                        // sim = Sim::Simulation(Sim::Simulation::SimSingleThrow,
+                        //                       textureShader,
+                        //                       colorShader);
                         break;
                     case sb::Key::V:
                         {
@@ -383,13 +514,13 @@ int main()
                         displayHelp = !displayHelp;
                         break;
                     case sb::Key::F2:
-                        displaySimInfo = !displaySimInfo;
+                        // displaySimInfo = !displaySimInfo;
                         break;
                     case sb::Key::F3:
-                        displayBallInfo = !displayBallInfo;
+                        // displayBallInfo = !displayBallInfo;
                         break;
                     case sb::Key::F4:
-                        sim.toggleShowLauncherLines();
+                        // sim.toggleShowLauncherLines();
                         break;
                     case sb::Key::PrintScreen:
                         {
@@ -447,7 +578,7 @@ int main()
                 }
                 break;
             case sb::Event::WindowResized:
-                crosshair.setScale(0.01f, 0.01f * ((float)e.data.wndResize.width / (float)e.data.wndResize.height), 0.01f);
+                // crosshair.setScale(0.01f, 0.01f * ((float)e.data.wndResize.width / (float)e.data.wndResize.height), 0.01f);
                 break;
             case sb::Event::WindowClosed:
                 wnd.close();
@@ -458,15 +589,15 @@ int main()
         }
 
         // physics update
-        uint32_t guard = 3u;
-        while ((deltaTime.getValue() >= physicsUpdateStep) && guard--)
-        {
-            sim.update(physicsUpdateStep);
-            deltaTime.update(-physicsUpdateStep);
+        // uint32_t guard = 3u;
+        // while ((deltaTime.getValue() >= physicsUpdateStep) && guard--)
+        // {
+        //     sim.update(physicsUpdateStep);
+        //     deltaTime.update(-physicsUpdateStep);
 
-            throwVelocity.update();
-            windVelocity.update();
-        }
+        //     throwVelocity.update();
+        //     windVelocity.update();
+        // }
 
         // drawing
         wnd.getCamera().moveRelative(Vec3(strafeSpeed, ascendSpeed, moveSpeed));
@@ -483,20 +614,37 @@ int main()
         // environment
         wnd.draw(skybox);
         wnd.draw(terrain);
-        wnd.draw(tower);
         
+
+        // float h_angle = wnd.getCamera().getHorizontalAngle().value();
+        // float v_angle = wnd.getCamera().getVerticalAngle().value();
+        // shark.setPosition(wnd.getCamera().getEye());//, + Vec3(sin(h_angle) * 2.0, -0.5f + sin(v_angle), cos(h_angle) * 2.0));
+        // shark.rotate(Vec3(0.0, 1.0, 0.0), Radians(v_angle/57.0));
+
+        // wnd.draw(shark);
+
+        for(sb::Fish &fish : shoalOfFish) {
+            fish.setVelocity(calculateVelocity(fish, wnd.getCamera().getEye()));
+            animateFish(fish);
+        }
+
+        for(sb::Fish &fish : shoalOfFish) {
+            fish.setPosition(calculatePosition(fish));
+            wnd.draw(fish);
+        }        
+
         // axes - disable edpth test to prevent blinking
         wnd.getRenderer().enableFeature(sb::Renderer::FeatureDepthTest, false);
-        wnd.draw(xaxis);
-        wnd.draw(yaxis);
-        wnd.draw(zaxis);
+        // wnd.draw(xaxis);
+        // wnd.draw(yaxis);
+        // wnd.draw(zaxis);
         wnd.getRenderer().enableFeature(sb::Renderer::FeatureDepthTest);
 
         // balls & forces
-        sim.drawAll(wnd.getRenderer());
+        // sim.drawAll(wnd.getRenderer());
 
         // crosshair
-        wnd.draw(crosshair);
+        // wnd.draw(crosshair);
 
 #if 0
         // info strings
